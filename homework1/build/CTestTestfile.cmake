19d1b5a5e4e7e396bcdc0a3e686445c3c40b896c
F4mpS <sany2k13@mail.ru>
2023-07-08 20:32:56 +0300

    Revert "Delete build folder"
    This reverts commit fc8d7d2b7b005927b598adc17f39f8013bae89fd.

---
+++ homework1/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Alexander/Desktop/OTUS/homework1
+# Build directory: C:/Users/Alexander/Desktop/OTUS/homework1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test "test")
+set_tests_properties(test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Alexander/Desktop/OTUS/homework1/CMakeLists.txt;119;add_test;C:/Users/Alexander/Desktop/OTUS/homework1/CMakeLists.txt;0;")
+subdirs("googletest")
